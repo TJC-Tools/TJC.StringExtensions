98de0d61f544fe93c709bde3aae3951f1e3456ec
Tyler <tylercarrol@hotmail.com>
2024-07-23 21:19:24 -0400

    Add Separator & Line Extensions (#8)

---
+++ TJC.StringExtensions/Lines/LineExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace TJC.StringExtensions.Lines;
+
+public static class LinesExtension
+{
+    private const int MaxLineWidth = 60;
+
+    public static IEnumerable<string> SplitNewLine(this string inputString) =>
+        inputString.ReplaceLineEndings().Split(Environment.NewLine);
+
+    public static List<string> SplitLines(this string? text, int width = MaxLineWidth, char separator = ' ')
+    {
+        var list = new List<string> { string.Empty };
+        if (text == null)
+            return list;
+
+        var array = text.Split(separator);
+        foreach (string word in array)
+            list.AddWord(word, width, separator);
+
+        return list;
+    }
+
+    private static void AddWord(this List<string> lines, string word, int width, char separator)
+    {
+        word = word.ReplaceLineEndings();
+        if (!word.Contains(Environment.NewLine))
+        {
+            if (word.Length + lines[^1].Length > width)
+                lines.Add(string.Empty);
+
+            lines[^1] += string.Concat(word, separator);
+            return;
+        }
+
+        var array = word.Split(Environment.NewLine);
+        lines.AddWord(array[0], width, separator);
+        foreach (string item in array.Skip(1))
+        {
+            lines.Add(string.Empty);
+            lines.AddWord(item, width, separator);
+        }
+    }
+
+    public static string RemoveMultipleBlankLines(this string text) =>
+        text.SplitNewLine().RemoveMultipleBlankLines().JoinNewLine();
+
+    public static List<string> RemoveMultipleBlankLines(this IEnumerable<string> lines)
+    {
+        var lastLineWasBlank = false;
+        var list = new List<string>();
+        foreach (string item in lines.Where((string line) => !string.IsNullOrWhiteSpace(line) || !lastLineWasBlank))
+        {
+            list.Add(item);
+            lastLineWasBlank = string.IsNullOrWhiteSpace(item);
+        }
+
+        return list;
+    }
+}
---
+++ TJC.StringExtensions/Separator/SeparatorExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace TJC.StringExtensions.Separator;
+
+public static class SeparatorExtensions
+{
+    #region Join Comma & Final Separator
+
+    public static string JoinComma(this IEnumerable<string> strings) =>
+        strings.JoinFinalSeparator(", ", ", ");
+
+    public static string JoinCommaOr(this IEnumerable<string> strings, bool orWithComma = false) =>
+        strings.JoinFinalSeparator(", ", $"{(orWithComma ? "," : string.Empty)} or ");
+
+    public static string JoinCommaAnd(this IEnumerable<string> strings, bool andWithComma = false) =>
+        strings.JoinFinalSeparator(", ", $"{(andWithComma ? "," : string.Empty)} and ");
+
+    public static string JoinCommaAmpersand(this IEnumerable<string> strings, bool andWithComma = false) =>
+        strings.JoinFinalSeparator(", ", $"{(andWithComma ? "," : string.Empty)} & ");
+
+    public static string JoinFinalSeparator(this IEnumerable<string> strings, string separator, string finalSeparator)
+    {
+        var list = strings.ToList();
+        return string.Join(separator, list.Take(list.Count - 1)) + (list.Count <= 1 ? string.Empty : finalSeparator) + list.LastOrDefault();
+    }
+
+    #endregion
+
+    #region Join List (New Line)
+
+    public static string JoinBullet(this IEnumerable<string> strings) =>
+        strings.JoinNewLine("\u2022");
+
+    public static string JoinNewLine(this IEnumerable<string> strings, string? prefix = null) =>
+        $"{prefix}{string.Join($"\n{prefix}", strings)}";
+
+    public static string JoinNewLineWithSpaces(this IEnumerable<string> strings, int spaces = 4) =>
+        string.Join($"\n{new string(' ', spaces)}", strings);
+
+    #endregion
+
+    #region Join Dictionary
+
+    public static string JoinBoth<T1, T2>(this IDictionary<T1, T2> dictionary, string separator = ", ", string keyValueSeparator = ": ")
+        where T1 : notnull
+        where T2 : notnull =>
+        string.Join(separator, dictionary.Select(x => $"{x.Key}{keyValueSeparator}{x.Value}"));
+
+    public static string JoinBothReverse<T1, T2>(this IDictionary<T1, T2> dictionary, string separator = ", ", string valueKeySeparator = ": ")
+        where T1 : notnull
+        where T2 : notnull =>
+        string.Join(separator, dictionary.Select(x => $"{x.Value}{valueKeySeparator}{x.Key}"));
+
+    public static string JoinKey<T1, T2>(this IDictionary<T1, T2> dictionary, string separator = ", ")
+        where T1 : notnull
+        where T2 : notnull =>
+        string.Join(separator, dictionary.Select(x => x.Key).ToList());
+
+    public static string JoinValue<T1, T2>(this IDictionary<T1, T2> dictionary, string separator = ", ")
+        where T1 : notnull
+        where T2 : notnull =>
+        string.Join(separator, dictionary.Select(x => x.Value).ToList());
+
+    #endregion
+}
