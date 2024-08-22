f796d3cb102de789ffe39bdf7685cb346cd8bc95
Tyler Carrol <tylercarrol@hotmail.com>
2024-08-21 21:58:57 -0400

    Create extension method to convert lines into a nicely formatted header
    primarily for use in logs or console applications

---
+++ TJC.StringExtensions.Tests/Header/HeaderExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using TJC.StringExtensions.Header;
+
+namespace TJC.StringExtensions.Tests.Header;
+
+[TestClass]
+public class HeaderExtensionsTests
+{
+    [TestMethod]
+    public void GetHeader()
+    {
+        // Arrange
+        var headerLines = new List<string> { "Title", "This is a description", "and this is another line" };
+
+        // Act
+        var result = headerLines.GenerateHeader().ToList();
+
+        // Assert
+        Assert.AreEqual(5, result.Count);
+        Assert.AreEqual("####################################", result[0]);
+        Assert.AreEqual("###            Title             ###", result[1]);
+        Assert.AreEqual("###    This is a description     ###", result[2]);
+        Assert.AreEqual("###   and this is another line   ###", result[3]);
+        Assert.AreEqual("####################################", result[4]);
+    }
+}
---
+++ TJC.StringExtensions/Header/HeaderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using TJC.StringExtensions.Lines;
+using TJC.StringExtensions.Padding;
+
+namespace TJC.StringExtensions.Header
+{
+    public static class HeaderExtensions
+    {
+        public static IEnumerable<string> GenerateHeader(this IEnumerable<string> lines, int internalLineLimit = 70, char character = '#', string linePrefix = "###   ", string lineSuffix = "   ###")
+        {
+            var tempLines = new List<string>();
+            // Get lines
+            foreach (var line in lines)
+                foreach (var splitLine in line.SplitNewLine())
+                    foreach (var chunk in splitLine.SplitChunks(internalLineLimit))
+                        tempLines.Add(chunk);
+
+            // Get border
+            var longestLine = tempLines.Max(x => x.Length);
+            var headerBorder = new string(character, longestLine + linePrefix.Length + lineSuffix.Length);
+
+            // Get lines with prefix and suffix
+            var headerLines = new List<string> { headerBorder };
+            foreach (var line in tempLines)
+                headerLines.Add($"{linePrefix}{line.PadBoth(longestLine)}{lineSuffix}");
+            headerLines.Add(headerBorder);
+            return headerLines;
+        }
+    }
+}
