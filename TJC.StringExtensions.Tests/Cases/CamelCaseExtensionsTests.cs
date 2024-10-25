2807b8625be9bca05a1a16082365ee9e12c3e8c5
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-24 20:59:24 -0400

    🚨 tests: Create Split Camel Case Tests

---
+++ TJC.StringExtensions.Tests/Cases/CamelCaseExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using TJC.StringExtensions.Cases;
+
+namespace TJC.StringExtensions.Tests.Cases;
+
+[TestClass]
+public class CamelCaseExtensionsTests
+{
+    [TestMethod]
+    public void SplitCamelCase_FirstLetterLowercase_SplitsWithSpace()
+    {
+        // Arrange
+        var input = "camelCaseExtensions";
+        var expected = "camel Case Extensions";
+
+        // Act
+        var result = input.SplitCamelCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void SplitCamelCase_FirstLetterUppercase_SplitsWithSpace()
+    {
+        // Arrange
+        var input = "CamelCaseExtensions";
+        var expected = "Camel Case Extensions";
+
+        // Act
+        var result = input.SplitCamelCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
