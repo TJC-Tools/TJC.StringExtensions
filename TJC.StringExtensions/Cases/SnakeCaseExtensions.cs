1b7903447762be8e3c1f6edc118dfba1eec5bd87
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-25 19:45:19 -0400

    Create ToSnakeCase Extension

---
+++ TJC.StringExtensions/Cases/SnakeCaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TJC.StringExtensions.Cases;
+
+/// <summary>
+/// Snake case extensions for strings.
+/// <para></para>
+/// Example of snake case: "snake-case-extensions"
+/// </summary>
+public static class SnakeCaseExtensions
+{
+    /// <summary>
+    /// Convert any case format to snake case.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns>snake_case</returns>
+    public static string ToSnakeCase(this string input) =>
+        string.Join("_", input.CodeCaseToWords()).ToLower();
+}
