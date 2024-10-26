69aa291e27687ec84d5cbe06d96850ffbff1832e
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-25 20:24:43 -0400

    🚨 tests: Add Kebab Case Tests

--- TJC.StringExtensions.Tests/Cases/KebabCaseExtensionsTests.cs
+++ TJC.StringExtensions.Tests/Cases/KebabCaseExtensionsTests.cs
@@ -18,4 +18,74 @@ public class KebabCaseExtensionsTests
         // Assert
         Assert.AreEqual(expected, result);
     }
+
+    [TestMethod]
+    public void FromPascalCase_ToKebabCase()
+    {
+        // Arrange
+        var input = "KebabCaseExtensions";
+        var expected = "kebab-case-extensions";
+
+        // Act
+        var result = input.ToKebabCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void FromCamelCase_ToKebabCase()
+    {
+        // Arrange
+        var input = "kebabCaseExtensions";
+        var expected = "kebab-case-extensions";
+
+        // Act
+        var result = input.ToKebabCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void FromTrainCase_ToKebabCase()
+    {
+        // Arrange
+        var input = "Kebab-Case-Extensions";
+        var expected = "kebab-case-extensions";
+
+        // Act
+        var result = input.ToKebabCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void FromSnakeCase_ToKebabCase()
+    {
+        // Arrange
+        var input = "kebab_case_extensions";
+        var expected = "kebab-case-extensions";
+
+        // Act
+        var result = input.ToKebabCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void FromKebabCase_ToKebabCase()
+    {
+        // Arrange
+        var input = "kebab-case-extensions";
+        var expected = "kebab-case-extensions";
+
+        // Act
+        var result = input.ToKebabCase();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
 }
